46b6cdb71d987dcbba8ec46fe7dd6a198b08c8c7
BrunZo <ziger.bruno@gmail.com>
2022-03-29 10:14:32 -0300

    merged Arduino nodes

--- arduino/nodos_Arduino/nodos_Arduino.ino
+++ arduino/nodos_Arduino/nodos_Arduino.ino
@@ -1,4 +1,6 @@
 #include <ros.h>
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
 #include <geometry_msgs/Twist.h>
 
 #define L_PWM 2
@@ -7,12 +9,16 @@
 #define R_BACK 5
 #define R_FORW 6
 #define R_PWM 7
+#define ECHO_PIN 8
+#define TRIG_PIN 9
 
 #define PWM_MIN 300
 #define PWMRANGE 500
 
+sensor_msgs::Range range_msg;
 ros::NodeHandle nh;
 ros::Subscriber<geometry::Twist> sub("/cmd_vel", &on_twist);
+ros::Publisher pub_range("/ultrasound", &range_msg);
 
 // Maps x from [0, 1] to [PWM_MIN, PWMRANGE]
 float map_pwm(float x) {
@@ -40,12 +46,32 @@ void setup() {
   pinMode(R_FORW, OUTPUT);
   pinMode(R_BACK, OUTPUT);
   pinMode(R_PWM, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
   nh.initNode();
   nh.subscribe(sub);
+  nh.advertise(pub_range);
+
+  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg.header.frame_id = frameid;
+  range_msg.field_of_view = 0.1; // fake?
+  range_msg.min_range = 0.0;
+  range_msg.max_range = 10.0;
 }
 
+float range_time = 0;
 void loop() {
+  if (millis() >= range_time) { // medio feo
+    if (millis() <= 10) 
+      digitalWrite(TRIG_PIN, HIGH);
+    else {
+      digitalWrite(TRIG_PIN, LOW);
+      range_msg.range = distance = pulseIn(ECHO_PIN, HIGH) * 0.0034 / 2;
+      range_msg.header.stamp = nh.now();
+      pub_range.publish(&range_msg);
+      range_time = millis() + 50;
+    }
+  }
   nh.spinOnce();
-  delay(10);
 }
